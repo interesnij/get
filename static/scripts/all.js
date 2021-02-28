542ec7226027b292c7c3a15f72f3f2ff22323551
Serg <ochkarik1983@mail.ru@>
2021-02-28 14:53:45 +0300

    J

--- static/scripts/all.js
+++ static/scripts/all.js
@@ -443,7 +443,7 @@ function service_tab_action(is, tab_class){
   tabs_items = tabs.querySelectorAll(".tab-pane");
   for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active")};
   cur = tabs.querySelector(tab_class);
-  cur.classList.add("active"); cur.slideDown("slow", "easeOutQuart")
+  cur.classList.add("active"); cur.fadeIn()
 }}
 
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
