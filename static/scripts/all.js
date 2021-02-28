24a6b928f2211b73b1ae73fefd1d93128c4e532e
Serg <ochkarik1983@mail.ru@>
2021-02-28 14:44:26 +0300

    J

--- static/scripts/all.js
+++ static/scripts/all.js
@@ -461,11 +461,12 @@ function service_tab_action(is, tab_class){
   nav = is.parentElement;
   nav_items = nav.querySelectorAll(".yy");
   for (var i = 0; i < nav_items.length; i++){nav_items[i].classList.remove("active"); nav_items[i].classList.add("pointer")};
-  is.classList.add("active");
+  is.classList.add("active"); is.classList.remove("pointer");
   tabs = nav.nextElementSibling;
   tabs_items = tabs.querySelectorAll(".tab-pane");
-  for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active"); nav_items[i].classList.remove("pointer")};
-  tabs.querySelector(tab_class).classList.add("active");
+  for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active")};
+  cur = tabs.querySelector(tab_class);
+  cur.classList.add("active")
 }}
 
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
