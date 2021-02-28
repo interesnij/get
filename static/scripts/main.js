caf99f52dd9d0dbde8a1e06e62c5e867abb49beb
Serg <ochkarik1983@mail.ru@>
2021-02-28 19:22:57 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -12,10 +12,11 @@ function service_tab_action(is, tab_class){
 }}
 
 function banner_height_init(){
+  console.log(window.innerHeight)
   if (document.body.querySelector(".js-height-full")){
         document.body.querySelector(".js-height-full").style.height = window.innerHeight
     }
-}
+};
 
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
 
