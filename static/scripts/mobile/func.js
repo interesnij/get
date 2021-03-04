d430c923a3b3bdb6d3c7b908df4f2cda66448ef7
Serg <ochkarik1983@mail.ru@>
2021-03-04 10:41:20 +0300

    J

---
+++ static/scripts/mobile/func.js
@@ -0,0 +1,138 @@
+class ToastManager {
+    constructor() {
+        this.id = 0;
+        this.toasts = [];
+        this.icons = {
+            'SUCCESS': "",
+            'ERROR': '',
+            'INFO': '',
+            'WARNING': '',
+        };
+        var body = document.querySelector('#ajax');
+        this.toastsContainer = document.createElement('div');
+        this.toastsContainer.classList.add('toasts', 'border-0');
+        body.appendChild(this.toastsContainer)
+    }
+    showSuccess(message) {
+        return this._showToast(message, 'SUCCESS')
+    }
+    showError(message) {
+        return this._showToast(message, 'ERROR')
+    }
+    showInfo(message) {
+        return this._showToast(message, 'INFO')
+    }
+    showWarning(message) {
+        return this._showToast(message, 'WARNING')
+    }
+    _showToast(message, toastType) {
+        var newId = this.id + 1;
+        var newToast = document.createElement('div');
+        newToast.style.display = 'inline-block';
+        newToast.classList.add(toastType.toLowerCase());
+        newToast.classList.add('toast');
+        newToast.innerHTML = `<progress max="100"value="0"></progress><h3>${message}</h3>`;
+        var newToastObject = {
+            id: newId,
+            message,
+            type: toastType,
+            timeout: 4000,
+            progressElement: newToast.querySelector('progress'),
+            counter: 0,
+            timer: setInterval(() => {
+                newToastObject.counter += 1000 / newToastObject.timeout;
+                newToastObject.progressElement.value = newToastObject.counter.toString();
+                if (newToastObject.counter >= 100) {
+                    newToast.style.display = 'none';
+                    clearInterval(newToastObject.timer);
+                    this.toasts = this.toasts.filter((toast) => {
+                        return toast.id === newToastObject.id
+                    })
+                }
+            }, 10)
+        }
+        newToast.addEventListener('click', () => {
+            newToast.style.display = 'none';
+            clearInterval(newToastObject.timer);
+            this.toasts = this.toasts.filter((toast) => {
+                return toast.id === newToastObject.id
+            })
+        });
+        this.toasts.push(newToastObject);
+        this.toastsContainer.appendChild(newToast);
+        return this.id++
+    }
+}
+
+function toast_success(text) {
+    var toasts = new ToastManager();
+    toasts.showSuccess(text)
+}
+
+function toast_error(text) {
+    var toasts = new ToastManager();
+    toasts.showError(text)
+}
+
+function toast_info(text) {
+    var toasts = new ToastManager();
+    toasts.showInfo(text)
+}
+
+function toast_warning(text) {
+    var toasts = new ToastManager();
+    toasts.showWarning(text)
+}
+
+function service_tab_action(is, tab_class){
+  if (!is.classList.contains("active")){
+  nav = is.parentElement;
+  nav_items = nav.querySelectorAll(".yy");
+  for (var i = 0; i < nav_items.length; i++){nav_items[i].classList.remove("active"); nav_items[i].classList.add("pointer")};
+  is.classList.add("active"); is.classList.remove("pointer");
+  tabs = nav.nextElementSibling;
+  tabs_items = tabs.querySelectorAll(".tab-pane");
+  for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active")};
+  cur = tabs.querySelector(tab_class);
+  cur.classList.add("active")
+}}
+
+function banner_height_init(block){
+  console.log(block);
+  if (block.querySelector(".js-height-full")){
+        block.querySelector(".js-height-full").style.height = window.innerHeight + 40 + "px";
+    }
+};
+
+function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
+
+function ajax_get_reload(url) {
+  var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
+    ajax_link.open( 'GET', url, true );
+    ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    ajax_link.onreadystatechange = function () {
+      if ( this.readyState == 4 && this.status == 200 ) {
+        elem_ = document.createElement('span');
+        elem_.innerHTML = ajax_link.responseText;
+        ajax = elem_.querySelector("#reload_block");
+        rtr = document.getElementById('ajax');
+        rtr.innerHTML = ajax.innerHTML;
+        window.scrollTo(0,0);
+        document.title = elem_.querySelector('title').innerHTML;
+        window.history.pushState({route: url}, "network", url);
+        banner_height_init(rtr);
+      }
+    }
+    ajax_link.send();
+}
+
+function init_wow(){
+    var wow = new WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 90,
+      mobile: false,
+      live: true
+  });
+  wow.init();
+}
---
+++ static/scripts/mobile/main.js
@@ -0,0 +1,41 @@
+
+ajax = document.body.querySelector("#ajax");
+banner_height_init(ajax);
+init_wow();
+
+on('#ajax', 'click', '.s_1', function() {
+  service_tab_action(this, ".tab_1")
+});
+on('#ajax', 'click', '.s_2', function() {
+  service_tab_action(this, ".tab_2")
+});
+on('#ajax', 'click', '.s_3', function() {
+  service_tab_action(this, ".tab_3")
+});
+on('#ajax', 'click', '.s_4', function() {
+  service_tab_action(this, ".tab_4")
+});
+on('#ajax', 'click', '.s_5', function() {
+  service_tab_action(this, ".tab_5")
+});
+
+on('body', 'click', '.ajax', function(event) {
+  event.preventDefault();
+  var url = this.getAttribute('href');
+  if (url != window.location.pathname){
+    ajax_get_reload(url)
+  } else {toast_info("Вы уже на этой странице")}
+})
+
+on('body', 'mouseover', '.mn-has-sub', function(event) {
+  this.nextElementSibling.style.display = "block"
+});
+on('body', 'mouseover', '.mn-sub', function(event) {
+  this.style.display = "block"
+});
+on('body', 'mouseout', '.mn-has-sub', function(event) {
+  this.nextElementSibling.style.display = "none"
+})
+on('body', 'mouseout', '.mn-sub', function(event) {
+  this.style.display = "none"
+})
