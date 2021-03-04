ebbc6fafbb0ed302ee228201948a0728270be0eb
Serg <ochkarik1983@mail.ru@>
2021-03-04 15:31:29 +0300

    J

--- static/scripts/mobile/main.js
+++ static/scripts/mobile/main.js
@@ -2,6 +2,7 @@
 ajax = document.body.querySelector("#ajax");
 banner_height_init(ajax);
 init_wow();
+loader_hide();
 
 on('#ajax', 'click', '.s_1', function() {
   service_tab_action(this, ".tab_1")
