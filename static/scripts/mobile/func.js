29f8e71b8805a0937741df257b6c76fb85d5899b
Serg <ochkarik1983@mail.ru@>
2021-03-04 14:56:24 +0300

    J

--- static/scripts/mobile/func.js
+++ static/scripts/mobile/func.js
@@ -103,7 +103,7 @@ function banner_height_init(block){
   console.log(block);
   if (block.querySelector(".js-height-full")){
     div = block.querySelector(".js-height-full");
-        div.style.height = (window.innerHeight - 50) + "px";
+        div.style.height = (window.innerHeight - 46) + "px";
     }
 };
 
